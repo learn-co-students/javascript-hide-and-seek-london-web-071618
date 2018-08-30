f9fea088f8b78268b6e5850e55f918d3b8589661
ritz-w <ritz.wu@outlook.com>
2018-08-30 13:51:31 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+  let jam = document.querySelectorAll('ul.ranked-list li')
+  for (let i=0; i<jam.length; i++) {
+    jam[i].innerHTML = parseInt(jam[i].innerHTML) + n
+  }
+}
