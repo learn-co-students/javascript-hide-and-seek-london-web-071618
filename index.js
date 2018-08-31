3ef4ed2c18e1cf1f17764c416570a95f26d266bb
Casey Van-Roose <caseyvanroose@Caseys-MacBook-Pro.local>
2018-08-31 09:20:27 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+function getFirstSelector(selector) {
 
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, length = rankedLists.length; i < length; i++) {
+
+    let children = rankedLists[i].children;
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+console.log(node)
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+console.log(node)
+  return node;
+}
+// increaseRankBy(1)
+deepestChild()
