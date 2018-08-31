9b89d16eeedd0ec4885704d6226eecf54e284953
Adham <admuaq@gmail.com>
2018-08-31 20:02:22 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+const getFirstSelector = (selector) => document.querySelector(selector)
+
+const nestedTarget = () => document.querySelector("#nested .target")
+// const nestedTarget = () => document.getElementById('nested').children[0].querySelectorAll('div')[2]
+
+const deepestChild = () => document.getElementById('grand-node').children[0].querySelectorAll('div')[2]
+
+const increaseRankBy = (n) => {
+    const list = document.getElementsByClassName('ranked-list')
+    for (let i =0; i < list.length; i++) {
+        list[i].innerHTML = (i +n )
+    }
+}
